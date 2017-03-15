1498d7661b4e2d795d65ab5b5514644f92ac79da
geoloep <contact@geoloep.nl>
2017-03-15 15:38:07 +0100

    tests for WMTS

---
+++ spec/sources/wmts/layer.js
@@ -0,0 +1,21 @@
+describe('WMTSLayer', function() {
+    var s = krite.Sources.WMTSSource;
+
+    // This should point to a wfs data source
+    var source = new s('https://geodata.nationaalgeoregister.nl/wmts/');
+    var layername = 'brtachtergrondkaart';
+
+    var layer;
+
+    beforeAll(function(done) {
+        source.getLayer(layername).then(function(l) {
+            layer = l;
+            done();
+        });
+    });
+
+    it('it should be defined', function() {
+        expect(layer).toBeDefined();
+        expect(layer.title).toBe(layername);
+    });
+});
---
+++ spec/sources/wmts/source.js
@@ -0,0 +1,24 @@
+describe('WMTSSource', function() {
+    var s = krite.Sources.WMTSSource;
+    var source = new s('https://geodata.nationaalgeoregister.nl/wmts/');
+    var layername = 'brtachtergrondkaart';
+
+    it('should be defined', function() {
+        expect(s).toBeDefined();
+    });
+
+    it('should have the layer name', function(done) {
+        source.getLayerNames().then(function(layers) {
+            expect(layers.indexOf(layername)).toBeGreaterThan(-1);
+            done();
+        });
+    });
+
+    it('should return a layer by name', function(done) {
+        source.getLayer(layername).then(function(layer) {
+            expect(layer.title).toBe(layername);
+            done();
+        });
+    });
+
+});
