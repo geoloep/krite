70a89db50095f609a35a28d5cabffc994f77f843
geoloep <contact@geoloep.nl>
2017-05-01 16:02:23 +0200

    ImageLayer

--- src/apps/index.ts
+++ src/apps/index.ts
@@ -1,6 +1,6 @@
 // export { BasemapApp  } from './basemap/basemap';
 export { ControlApp  } from './control/app';
-export { InspectorApp  } from './inspector/inspector';
+// export { InspectorApp  } from './inspector/inspector';
 // export { LegendApp  } from './legend/legend';
 export { PdokSearchApp } from './pdokSearch/app';
 export { HelloWorldApp } from './helloWorld/app';
