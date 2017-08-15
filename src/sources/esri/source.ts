689616d6775041e19db06afff3c63104d5ebd154
geoloep <contact@geoloep.nl>
2017-08-15 13:10:59 +0200

    Fix Esrisource error handling

--- src/sources/esri/source.ts
+++ src/sources/esri/source.ts
@@ -51,10 +51,10 @@ export class ESRISource implements IDataSource {
         for (let service of this.services.services) {
             layerNames.push(service.name);
         }
-        return(layerNames);
+        return (layerNames);
     }
 
-    async getLayer(name: string) {
+    async getLayer(name: string): Promise<ILayer> {
         if (!this.services) {
             await this.getServices();
         }
@@ -62,74 +62,37 @@ export class ESRISource implements IDataSource {
         return await this._getLayer(name);
     }
 
-    private async _getLayer(name: string) {
-        if (this.services.services) {
-            let service: IESRIServiceListing | undefined = undefined;
+    private async _getLayer(name: string): Promise<ILayer> {
+        let service: IESRIServiceListing | undefined = undefined;
 
-            for (let serviceItem of this.services.services) {
-                if (serviceItem.name === name) {
-                    service = serviceItem;
-                }
+        for (let serviceItem of this.services.services) {
+            if (serviceItem.name === name) {
+                service = serviceItem;
             }
-
-            if (service) {
-                let serviceUrl = `${this.url}/${name}/${service.type}/`;
-
-                let response = await fetch(serviceUrl + url.format({
-                        query: {
-                            f: 'pjson',
-                        },
-                    })
-                );
-
-                if (!response.ok) {
-                    console.error('Malformed response');
-                }
-
-                let json = await response.json();
-
-                this.layers[name] = new this.typeToLayer[service.type](serviceUrl, name, json, this);
-
-                return this.layers[name];
-            } else {
-                console.error('Layer not found');
-            }
-        } else {
-            console.error('Services not loaded correctly');
         }
 
+        if (service) {
+            let serviceUrl = `${this.url}/${name}/${service.type}/`;
 
-        // return new Promise<ILayer>((resolve, reject) => {
-        //     for (let service of this.services.services) {
-        //         if (service.name === name) {
-        //             if (service.type in this.typeToLayer) {
-        //                 // Soms is de naam service/laag, we moeten alleen 'laag' toevoegen aan de url
-        //                 let urlName = name.split('/').pop();
-        //                 let fullUrl = `${this.url}${urlName}/${service.type}/` //this.url + urlName + '/';
+            let response = await fetch(serviceUrl + url.format({
+                query: {
+                    f: 'pjson',
+                },
+            }),
+            );
 
-        //                 fetch(
-        //                     fullUrl +
-        //                     url.format({
-        //                         query: {
-        //                             f: 'pjson',
-        //                         },
-        //                     })
-        //                 ).then((response) => {
-        //                     if (response.ok) {
-        //                         response.json().then((json) => {
-        //                             this.layers[name] = new this.typeToLayer[service.type](fullUrl, json, this);
-        //                             resolve(this.layers[name]);
-        //                         }).catch(reject);
-        //                     } else {
-        //                         reject();
-        //                     }
-        //                 }).catch(reject);
-        //             } else {
-        //                 reject(`No implementation for ${service.type}`);
-        //             }
-        //         }
-        //     }
-        // });
+            if (!response.ok) {
+                console.error('Malformed response');
+            }
+
+            let json = await response.json();
+
+            this.layers[name] = new this.typeToLayer[service.type](serviceUrl, name, json, this);
+
+            return this.layers[name];
+        } else {
+            throw('Layer not found');
+        }
     }
 
     private async getServices(): Promise<void> {
@@ -139,7 +102,7 @@ export class ESRISource implements IDataSource {
                 query: {
                     f: 'pjson',
                 },
-            })
+            }),
         );
 
         if (!response.ok) {
@@ -147,7 +110,6 @@ export class ESRISource implements IDataSource {
         }
 
         this.services = await response.json();
-
 
         return;
     }
