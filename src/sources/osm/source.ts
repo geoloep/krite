18ccf7e319bb51fe915b50a599b3174c93045b0e
geoloep <contact@geoloep.nl>
2017-08-23 16:20:59 +0200

    New datasource: osm

---
+++ src/sources/osm/source.ts
@@ -0,0 +1,47 @@
+import * as L from 'leaflet';
+
+import { IDataSource, ILayer } from '../../types';
+
+/**
+ * This layer contains the well known openstreetmap
+ */
+export class OsmLayer implements ILayer {
+    private _layer: L.TileLayer;
+    private _legend: string;
+
+    title = 'Openstreetmap';
+    name = 'Openstreetmap';
+    abstract = 'Welcome to OpenStreetMap, the project that creates and distributes free geographic data for the world.';
+    bounds = new L.LatLngBounds(L.latLng(-180, 90), L.latLng(180, 90));
+    preview = '<img src="http://tile.osm.org/0/0/0.png">';
+    
+    legend = '';
+
+    get leaflet() {
+        if (!this._layer) {
+            this._layer = L.tileLayer('http://tile.osm.org/{z}/{x}/{y}.png');
+        }
+
+        return this._layer;
+    }
+}
+
+
+/**
+ * This source exposes the regular openstreetmap tiles
+ */
+export class OsmSource implements IDataSource {
+    private instance: OsmLayer;
+
+    async getLayerNames() {
+        return ['Openstreetmap'];
+    }
+
+    async getLayer(name: string) {
+        if (!this.instance) {
+            this.instance = new OsmLayer();
+        }
+
+        return this.instance;
+    }
+}
