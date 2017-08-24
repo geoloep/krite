834432d4361bde9ac7b1fee6f3e3e366209ddb56
geoloep <contact@geoloep.nl>
2017-08-24 17:54:25 +0200

    Add first inspection function to esri layer

--- src/sources/esri/tiledMapLayer.ts
+++ src/sources/esri/tiledMapLayer.ts
@@ -3,12 +3,19 @@ import * as esri from 'esri-leaflet';
 import { ILayer } from '../../types';
 import { ESRISource } from './source';
 
+import pool from '../../servicePool';
+import { MapService } from '../../services/map';
+
+const map = pool.getService<MapService>('MapService');
+
 export class ESRITiledMapLayer implements ILayer {
     previewSet = 0;
     previewCol = 0;
     previewRow = 0;
 
-    private _leaflet: L.Layer;
+    hasOperations = true;
+
+    private _leaflet: any;
 
     constructor(readonly url: string, readonly capabilities: any) {
     }
@@ -46,5 +53,15 @@ export class ESRITiledMapLayer implements ILayer {
 
     get legend() {
         return ``;
+    }
+
+    async intersectsPoint?(point: L.Point) {
+        const features = await new Promise((resolve, reject) => {
+            this._leaflet.identify().on(map.map).at([point.x, point.y]).layers('top').run((error: boolean, features: any) => {
+                resolve(features);
+            });
+        });
+
+        return features;
     }
 }
