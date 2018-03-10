1b063040074558e289ac13ff7eea6c2d585f6cda
geoloep <contact@geoloep.nl>
2018-03-10 17:49:00 +0100

    Buma/Buefy version of pdoksearch component

---
+++ src/components/buefy/pdokSearch/pdokSearch.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="PdokSearchComponent">
+
+        <b-dropdown class="wide">
+            <b-field slot="trigger">
+                <b-input placeholder="Adres zoeken" type="search" v-model="searchString" @keydown.native.prevent.down="selectDown" @keydown.native.prevent.up="selectUp" @keydown.native.prevent.enter="selectEnter"></b-input>
+            </b-field>
+            <b-dropdown-item v-for="(result, key) in results" :key="key" @click="searchClick(result)" :class="result === highlighted ? 'is-active' : ''">{{result.weergavenaam}}</b-dropdown-item>
+        </b-dropdown>
+    </div>
+</template>
+
+<style>
+.PdokSearchComponent .wide, .dropdown-trigger {
+    width: 100%;
+}
+</style>
+
+<script src="./pdokSearch.js"></script>
