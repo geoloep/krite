34d385d89bd5a9827029c77fb9d5bc2f392c5567
geoloep <contact@geoloep.nl>
2018-04-23 11:09:28 +0200

    Drop seperate app wrappers

--- src/apps/index.ts
+++
@@ -1,7 +0,0 @@
-export { BasemapApp  } from './basemap/app';
-export { ControlApp  } from './control/app';
-export { HelloWorldApp } from './helloWorld/app';
-export { InspectorApp  } from './inspector/app';
-export { LegendApp  } from './legend/app';
-export { PdokSearchApp } from './pdokSearch/app';
-export { NominatimSearchApp } from './nominatimSearch/app';
