a8205fe606487a7a00c75577c8d11f1b88774e6c
geoloep <contact@geoloep.nl>
2018-04-26 14:21:55 +0200

    Pointer over basemaps

--- src/components/buefy/basemap/basemap.vue
+++ src/components/buefy/basemap/basemap.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="basemapComponent">
         <div v-for="(basemap, key) in basemaps" :key="key">
-            <div v-html="basemap.preview" @click="setBasemap(basemap)"></div>
+            <div class="basemap" v-html="basemap.preview" @click="setBasemap(basemap)"></div>
             <br>
         </div>
     </div>
 </template>
 
 <style scoped>
-
+.basemap {
+    cursor: pointer;
+}
 </style>
 
 <script src="./basemap.js"></script>
