1047e231d6d7c20def80f07e5d08a011032b76e7
ynte <contact@geoloep.nl>
2018-06-19 13:02:03 +0200

    disabe for now

--- src/main.ts
+++ src/main.ts
@@ -15,16 +15,16 @@ limitations under the License.
 */
 
 // Expose Leaflet
-import * as L from 'leaflet';
-(window as any).L = L;
+// import * as L from 'leaflet';
+// (window as any).L = L;
 
-import pool from './servicePool';
+// import pool from './servicePool';
 
-// import * as apps from './apps/index';
-import * as services from './services/index';
-import * as sources from './sources/index';
+// // import * as apps from './apps/index';
+// import * as services from './services/index';
+// import * as sources from './sources/index';
 
-export let Services = services;
-export let Sources = sources;
-// export let Apps = apps;
-export let ServicePool: services.ServiceManager = pool;
+// export let Services = services;
+// export let Sources = sources;
+// // export let Apps = apps;
+// export let ServicePool: services.ServiceManager = pool;
