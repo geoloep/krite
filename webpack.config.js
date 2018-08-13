5e61e0100b6d5bbbff7f772316571f45e520aa82
ynte <contact@geoloep.nl>
2018-08-13 11:54:11 +0200

    Update to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,29 @@
 var path = require('path')
 var webpack = require('webpack')
 
+const devMode = process.env.NODE_ENV !== 'production';
+
 module.exports = {
-  entry: './lib/main.js',
+  entry: './src/main.ts',
+  mode: process.env.NODE_ENV,
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
     filename: 'krite.js',
-    library: 'krite',
+    library: 'Krite',
     libraryTarget: 'umd',
   },
   module: {
     rules: [
       {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-          },
-          // other vue-loader options go here
-          esModule: true
-        }
-      },
-      {
-        test: /\.js$/,
-        parser: {
-          amd: false,
-        },
+        test: /\.ts$/,
+        loader: 'ts-loader',
       },
     ]
   },
   resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-    },
-    extensions: ['.js', '.vue'],
+    extensions: ['.js', '.ts'],
     symlinks: false,
   },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true
-  },
-  performance: {
-    hints: false
-  },
   devtool: '#eval-source-map'
-}
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = false
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
 }
